112d0d51c607c1646706c1b1254aa929b5d79e0e
afrlv1 <afrlv@yandex.ru>
2020-08-04 10:16:27 +0500

    Created urls files, added Admin room

---
+++ api/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Printer, Check
+
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ("name", "api_key", "check_type", "point_id")
+    list_filter = ["check_type"]
+
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ("printer_id", "type", "order", "status")
+    list_filter = ["printer_id", "type", "status"]
+
+
+admin.site.register(Printer, PrinterAdmin)
+admin.site.register(Check, CheckAdmin)
