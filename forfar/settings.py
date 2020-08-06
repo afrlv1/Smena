e3681306643d0f7bde4916d770dfe09763095f1a
afrlv1 <afrlv@yandex.ru>
2020-08-06 13:19:25 +0500

    Edited install apps

--- forfar/settings.py
+++ forfar/settings.py
@@ -21,9 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_yasg',
     'django_rq',
-    'checks',
+    'api',
 ]
 
 MIDDLEWARE = [
